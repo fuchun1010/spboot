a5f0a51c298d85170fd5caa50283d4cbb819d715
Tank <fuch@fullsample.com>
2017-10-26 13:22:41 +0800

    [fuchun] #Tech refactor

--- src/main/java/com/tank/controller/AsyncController.java
+++ src/main/java/com/tank/controller/AsyncController.java
@@ -1,23 +1,24 @@
 package com.tank.controller;
 
 import com.tank.dao.UserDAO;
-import com.tank.message.Address;
 import com.tank.message.User;
 import io.reactivex.Observable;
-import io.reactivex.Single;
-
-import static org.springframework.http.HttpStatus.*;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
+
+import static org.springframework.http.MediaType.*;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+
+import static org.springframework.web.bind.annotation.RequestMethod.*;
+
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.async.DeferredResult;
 
 import java.util.List;
+
+import static org.springframework.http.HttpStatus.OK;
 
 /**
  * @author fuchun
@@ -25,12 +26,13 @@ import java.util.List;
  */
 @CrossOrigin
 @RestController
+@RequestMapping(path = "/v1/api")
 public class AsyncController {
 
   @RequestMapping(
       path = "/async/user",
-      method = RequestMethod.GET,
-      produces = MediaType.APPLICATION_JSON_UTF8_VALUE
+      method = GET,
+      produces = APPLICATION_JSON_UTF8_VALUE
   )
   public DeferredResult<ResponseEntity<List<User>>> asyncUsers() {
     DeferredResult<ResponseEntity<List<User>>> deferredResult = new DeferredResult<>();
@@ -49,12 +51,12 @@ public class AsyncController {
 
   @RequestMapping(
       path = "/sync/user",
-      method = RequestMethod.GET,
-      produces = MediaType.APPLICATION_JSON_UTF8_VALUE
+      method = GET,
+      produces = APPLICATION_JSON_UTF8_VALUE
   )
   public ResponseEntity<List<User>> syncUsers() {
     final List<User> users = userDAO.findAll();
-    return new ResponseEntity<List<User>>(users, OK);
+    return new ResponseEntity<>(users, OK);
   }
 
   @Autowired
