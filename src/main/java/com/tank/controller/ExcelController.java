28e192fc1da43deb51a374b433decce0b5247a96
廖宇文 <liaoyuwen@ryousakibunsMBP.lan>
2017-12-14 16:12:43 +0800

    处理了schema文件, 加上可以生成createSchema所需的二维数组了

--- src/main/java/com/tank/controller/ExcelController.java
+++ src/main/java/com/tank/controller/ExcelController.java
@@ -3,7 +3,12 @@ package com.tank.controller;
 import com.tank.domain.DataType;
 import lombok.NonNull;
 import lombok.val;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -11,10 +16,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
@@ -63,34 +65,24 @@ public class ExcelController {
 
 
   //curl -i -X POST -H "Content-Type: multipart/form-data" -F "file=@test.mp3" http://mysuperserver/media/1234/upload/
-  @PostMapping(path = "/upload")
-  public ResponseEntity<Map<String, String>> uploadFile(@RequestParam MultipartFile file) {
+  @PostMapping(path = "/upload/{type}")
+  public ResponseEntity<List<List<String>>> uploadFile(@RequestParam MultipartFile file
+          , @PathVariable("type") String type) {
     Map<String, String> map = new HashMap<>();
-
-    File downloadDir = new File(downLoadDirPath());
-    List<File> existedFiles = Stream.of(downloadDir.listFiles()).filter(existedFile -> existedFile.getName().equalsIgnoreCase(file.getName())).collect(Collectors.toList());
-    if (existedFiles.size() == 0) {
-
-      try {
-        String fileName = downLoadDirPath() + file.getOriginalFilename();
-        File tmpFile = new File(fileName);
-        if (!tmpFile.exists()) {
-          tmpFile.createNewFile();
-        }
-        byte[] data = file.getBytes();
-        FileOutputStream out = new FileOutputStream(tmpFile);
-        out.write(data);
-        out.flush();
-        out.close();
-        map.putIfAbsent("status", "ok");
-        return new ResponseEntity<>(map, OK);
-      } catch (IOException e) {
-        e.printStackTrace();
-        map.putIfAbsent("errors", e.getMessage());
-        return new ResponseEntity<>(map, INTERNAL_SERVER_ERROR);
-      }
+    String path = uploadExcelAndGetPath(file);
+    if (path == null) {
+      map.putIfAbsent("errors", "upload failed.");
+        return new ResponseEntity<>(INTERNAL_SERVER_ERROR);
     }
-    return new ResponseEntity<>(map, OK);
+    if ("schema".equals(type)) {
+        List fieldsInfo = getSchemaData(path);
+        if (null != fieldsInfo) {
+            return new ResponseEntity<>(fieldsInfo, OK);
+        }
+    } else if ("data".equals(type)) {
+
+    }
+    return new ResponseEntity<>(INTERNAL_SERVER_ERROR);
   }
 
   private Map<String, DataType> fetchSchema(@NonNull final String filePath) {
@@ -129,14 +121,86 @@ public class ExcelController {
     return map;
   }
 
+  private List getSchemaData(String filePath) {
+      InputStream ExcelFileToRead = null;
+      HSSFWorkbook wb = null;
+      try {
+          ExcelFileToRead = new FileInputStream(filePath);
+          wb = new HSSFWorkbook(ExcelFileToRead);
+      } catch (FileNotFoundException e) {
+          e.printStackTrace();
+          return null;
+      } catch (IOException e) {
+          e.printStackTrace();
+          return null;
+      }
+      List<List<String>> fieldsList = new LinkedList<>();
+      HSSFSheet sheet=wb.getSheetAt(0);
+      HSSFRow row;
+      HSSFCell cell;
+      Iterator rows = sheet.rowIterator();
+      int rowNum = 0;
+      while (rows.hasNext())
+      {
+          rowNum++;
+          //只处理2-5行的
+          if (rowNum > 6) {
+              break;
+          }
+          if (1 == rowNum) {
+              rows.next();
+              continue;
+          }
+          row=(HSSFRow) rows.next();
+          Iterator cells = row.cellIterator();
 
-  private String downLoadDirPath() {
+          int colNum = 0;
+          while (cells.hasNext()) {
+
+              List<String> col;
+              if (fieldsList.size() <= colNum || (col = fieldsList.get(colNum)) == null) {
+                  col = new LinkedList<>();
+                  fieldsList.add(colNum, col);
+              }
+              cell=(HSSFCell) cells.next();
+              col.add(cell.toString());
+              colNum++;
+          }
+
+      }
+      return fieldsList.size() > 0 ? fieldsList : null;
+  }
+
+  private static String downLoadDirPath() {
     val file = new File(".");
     val absolutePath = file.getAbsolutePath().replace(".", "");
     val sb = new StringBuffer();
     sb.append(absolutePath);
-    sb.append("download/");
+    sb.append("download"+File.separator);
     return sb.toString();
+  }
+
+  static String uploadExcelAndGetPath(MultipartFile file) {
+    File downloadDir = new File(downLoadDirPath());
+    List<File> existedFiles = Stream.of(downloadDir.listFiles()).filter(existedFile -> existedFile.getName().equalsIgnoreCase(file.getName())).collect(Collectors.toList());
+    if (existedFiles.size() == 0) {
+      try {
+        String fileName = downLoadDirPath() + file.getOriginalFilename();
+        File tmpFile = new File(fileName);
+        if (!tmpFile.exists()) {
+          tmpFile.createNewFile();
+        }
+        byte[] data = file.getBytes();
+        FileOutputStream out = new FileOutputStream(tmpFile);
+        out.write(data);
+        out.flush();
+        out.close();
+        return fileName;
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+    return null;
   }
 
   private File fetchDownloadFile(@NonNull final String fileName) {
