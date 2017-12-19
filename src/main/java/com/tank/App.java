83f8d140cc359a4c78a327b235876106bf67757d
Tank <fuch@fullsample.com>
2017-12-19 10:42:47 +0800

    [fuchun] #Tech 完成根据单元格标识，计算当前位置，比如AA2 => 27

--- src/main/java/com/tank/App.java
+++ src/main/java/com/tank/App.java
@@ -1,13 +1,14 @@
 package com.tank;
 
-import lombok.val;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-import java.util.concurrent.*;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingDeque;
 
 /**
  * @author fuchun
@@ -20,12 +21,12 @@ public class App {
     ApplicationContext context = SpringApplication.run(App.class);
 
     BlockingQueue<String> queue = (BlockingQueue<String>) context.getBean("importSqlQueue");
-    JdbcTemplate oracleJdbcTemplate = (JdbcTemplate) context.getBean("oracleJdbcTemplate");
+    JdbcTemplate jdbcTemplate = (JdbcTemplate) context.getBean("oracleJdbcTemplate");
     Executors.newCachedThreadPool().execute(() -> {
       while (true) {
         try {
           String insertSql = queue.take();
-          oracleJdbcTemplate.execute(insertSql);
+          jdbcTemplate.execute(insertSql);
           System.out.println("inserted ok");
         } catch (InterruptedException e) {
           e.printStackTrace();
@@ -37,7 +38,7 @@ public class App {
 
   @Bean(name = "importSqlQueue")
   public BlockingQueue<String> importSqlQueue() {
-    return new ArrayBlockingQueue<String>(20);
+    return new LinkedBlockingDeque<>();
   }
 
 
--- src/main/java/com/tank/common/toolkit/ExcelToolkit.java
+++ src/main/java/com/tank/common/toolkit/ExcelToolkit.java
@@ -1,11 +1,10 @@
 package com.tank.common.toolkit;
 
-import com.tank.exception.ExcelNotFoundException;
 import lombok.NonNull;
 import lombok.val;
 import net.lingala.zip4j.core.ZipFile;
 import net.lingala.zip4j.exception.ZipException;
-import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import oracle.sql.CHAR;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -13,8 +12,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 /**
  * @author fuchun
@@ -22,7 +21,7 @@ import java.util.List;
 public class ExcelToolkit {
 
 
-  public static String generateZipExcel(File file) throws IOException {
+  public static String generateZipExcel(final @NonNull File file) throws IOException {
     String path = DirectoryToolKit.downloadDir();
     val zipFile = new File(path + File.separator + file.getName().replace("xlsx", "zip"));
     Path source = Paths.get(file.toURI());
@@ -33,7 +32,7 @@ public class ExcelToolkit {
     return zipFile.getAbsolutePath();
   }
 
-  public static String unZipExcel(String filePath) throws ZipException {
+  public static String unZipExcel(final @NonNull String filePath) throws ZipException {
     File zipFile = new File(filePath);
     String dirName = zipFile.getName().replace(".zip", "");
     val dirPath = DirectoryToolKit.downloadDir() + File.separator + dirName;
@@ -46,6 +45,93 @@ public class ExcelToolkit {
     return dirPath;
   }
 
+  /**
+   * 根据xml中c标记的字符串获取位置
+   *
+   * @param cellNo
+   * @return
+   */
+  public static Integer excelCellPosition(final @NonNull String cellNo) {
+    val mapped = createMapped();
+    val tmpCellNo = extractCharacter(cellNo.toUpperCase());
+    val queue = toQueue(tmpCellNo);
+    val result = calculatePosition(queue, mapped);
+    return result;
+  }
+
+  /**
+   * 创建映射关系，A-Z,A从1开始
+   *
+   * @return
+   */
+  private static Map<String, Integer> createMapped() {
+    val map = new HashMap<String, Integer>();
+    val start = 'A';
+    val end = 'Z';
+    int counter = 1;
+    for (char ch = start; ch <= end; ch++) {
+      map.putIfAbsent(String.valueOf(ch), counter);
+      counter++;
+    }
+    return map;
+  }
+
+  /**
+   * 过滤字符串中的数字类型
+   *
+   * @param cellNo
+   * @return
+   */
+  private static String extractCharacter(final @NonNull String cellNo) {
+    int len = cellNo.length();
+    StringBuffer sb = new StringBuffer();
+    for (int i = 0; i < len; i++) {
+      char ch = cellNo.charAt(i);
+      val isNumber = Character.isDigit(ch);
+      if (!isNumber) {
+        sb.append(ch);
+      }
+    }
+    return sb.toString();
+  }
+
+
+  /**
+   * 将位置字符串转换为队列AB=>A, B
+   *
+   * @param cellNo
+   * @return
+   */
+  private static Queue<String> toQueue(String cellNo) {
+    Queue<String> queue = new ConcurrentLinkedQueue<>();
+    int len = cellNo.length();
+    for (int i = 0; i < len; i++) {
+      char ch = cellNo.charAt(i);
+      queue.add(String.valueOf(ch));
+    }
+    return queue;
+  }
+
+  /**
+   * 计算单元格位置
+   * 输入AA 返回27
+   * 输入AAW 返回725
+   * @param queue
+   * @param map
+   * @return
+   */
+  private static Integer calculatePosition(Queue<String> queue, Map<String, Integer> map) {
+    int sum = 0;
+    int len = queue.size();
+    String data = queue.poll();
+    if (len > 1) {
+      sum += map.get(data) * Math.pow(26, len - 1);
+      return sum + calculatePosition(queue, map);
+    } else {
+      sum += map.get(data);
+      return sum;
+    }
+  }
 }
 
 
