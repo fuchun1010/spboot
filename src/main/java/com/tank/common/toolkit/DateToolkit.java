7fa726c196546fd6d6c3cef2ff3c3706143a6bb0
Tank <fuch@fullsample.com>
2017-12-20 10:58:28 +0800

    [fuchun] #Tech 删除一些无用的代码

--- src/main/java/com/tank/common/toolkit/DateToolkit.java
+++ src/main/java/com/tank/common/toolkit/DateToolkit.java
@@ -3,37 +3,16 @@ package com.tank.common.toolkit;
 import lombok.NonNull;
 import lombok.val;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 /**
  * @author fuchun
  */
 public class DateToolkit {
 
-  public static boolean isDate(@NonNull String dateStr) {
-    return isBasicDate(dateStr) || isDateWithTime(dateStr) ;
-  }
-
-  public static boolean isBasicDate(@NonNull String dateStr) {
-    val basicDatePattern = "\\d{4}-\\d{2}-\\d{2}";
-    if (dateStr.matches(basicDatePattern)) {
-      return true;
-    }
-    return false;
-  }
-
-  public static boolean isDateWithTime(@NonNull String dateStr) {
-    val datePattenWithTime = "\\d{4}-\\d{2}-\\d{2} ([0-1]\\d|2[0-3]):([0-5]\\d):([0-5]\\d)";
-    if (dateStr.matches(datePattenWithTime)) {
-      return true;
-    }
-    return false;
-  }
-
-  public static String toBasicDate(@NonNull String dateStr) {
-    return "TO_DATE('"+dateStr+"', 'YYYY-MM-DD')";
-  }
-
   public static String toDateWithTime(@NonNull String dateStr) {
-    return "TO_DATE('"+dateStr+"', 'YYYY-MM-DD HH24:mi:ss')";
+    return "TO_DATE('" + dateStr + "', 'YYYY-MM-DD HH24:mi:ss')";
   }
 
 }
--- src/main/java/com/tank/domain/ExcelCell.java
+++ src/main/java/com/tank/domain/ExcelCell.java
@@ -1,53 +1,26 @@
 package com.tank.domain;
 
 
+import static com.tank.common.toolkit.DateToolkit.*;
 import lombok.Data;
-import lombok.NonNull;
 import lombok.val;
 
-import javax.swing.text.DateFormatter;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
+/**
+ * @author fuchun
+ */
 @Data
 public class ExcelCell {
-
 
   private String value = "";
 
   private String type = "";
 
-  private String STRING_TYPE = "s";
-
   private String NUMBER_TYPE = "n";
 
   private String HEADER_TYPE = "h";
 
+  private String DATE_TYPE = "d";
 
-  /**
-   * 是否是不带小时分秒的日期类型
-   *
-   * @param dateStr
-   * @return
-   */
-  private boolean isDateWithOutHours(@NonNull String dateStr) {
-    val pattern = "\\d{4}-\\d{2}-\\d{2}";
-    return dateStr.matches(pattern);
-  }
-
-  private boolean isDateWithHours(@NonNull String dateStr) {
-    val formatter = "yyyy-MM-dd HH:mm:ss";
-    SimpleDateFormat sdf = new SimpleDateFormat(formatter);
-    try {
-      sdf.parse(dateStr);
-      return true;
-    } catch (ParseException e) {
-      return false;
-    }
-  }
 
   @Override
   public String toString() {
@@ -56,9 +29,9 @@ public class ExcelCell {
     if (isNumberTypeOrHeader) {
       return this.value;
     }
-    boolean isDate = isDateWithOutHours(this.value) || isDateWithHours(this.value);
+    boolean isDate = DATE_TYPE.equalsIgnoreCase(type);
     if (isDate) {
-      return "to_date(" + rs + ", 'yyyy-MM-dd hh24:mi:ss')";
+      return toDateWithTime(this.value);
     }
     return rs;
   }
